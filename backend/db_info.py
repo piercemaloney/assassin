54291b2d372ae9826991a53593d6490af3e4b6a9
piercemaloney <70864852+piercemaloney@users.noreply.github.com>
2023-02-27 10:38:51 -0500

    removing db_info from public repo

--- backend/db_info.py
+++
@@ -1,7 +0,0 @@
-# ---------------------------------------------------------------------
-# MongoDB info
-username = 'piercemaloney'
-password = 'hiXbnshm0cLwRQNN'
-uri = f'mongodb+srv://{username}:{password}@cluster0.4c1fnmr.mongodb.net/?retryWrites=true&w=majority'
-
-# ---------------------------------------------------------------------
